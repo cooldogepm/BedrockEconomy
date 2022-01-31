336ebde51f52b23218c7b8afedaf297e603c1f74
cooldogedev <developmentdoge@gmail.com>
2022-01-31 12:37:17 +0300

    unused property

--- src/cooldogedev/BedrockEconomy/account/AccountManager.php
+++ src/cooldogedev/BedrockEconomy/account/AccountManager.php
@@ -41,10 +41,6 @@ final class AccountManager extends BedrockEconomyOwned
      * @var Transaction[]
      */
     protected array $transactions;
-    /**
-     * @var string[]
-     */
-    protected array $cache;
 
     public function __construct(BedrockEconomy $plugin)
     {
