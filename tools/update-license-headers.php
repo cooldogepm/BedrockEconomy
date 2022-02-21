664db636eec02e6906cd58f4382bb701225991f0
Fadhel D <77399231+fadheldev@users.noreply.github.com>
2022-02-21 22:54:49 +0300

    use a new cs for the scripts

--- tools/update-license-headers.php
+++ tools/update-license-headers.php
@@ -1,26 +1,31 @@
 <?php
 
-$current_year = date('Y');
-$prev_year = $current_year - 1;
+function main(): void
+{
+    $current_year = date('Y');
+    $prev_year = $current_year - 1;
 
-$path = realpath("../src/cooldogedev/BedrockEconomy/");
+    $path = realpath("../src/cooldogedev/BedrockEconomy/");
 
-$files = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($path),
-    RecursiveIteratorIterator::LEAVES_ONLY
-);
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($path),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
 
-/**
- * @var SplFileInfo $file
- */
-foreach ($files as $file) {
-    if ($file->getFilename() === "." || $file->getFilename() === "..") {
-        continue;
+    /**
+     * @var SplFileInfo $file
+     */
+    foreach ($files as $file) {
+        if ($file->getFilename() === "." || $file->getFilename() === "..") {
+            continue;
+        }
+
+        $file_contents = file_get_contents($file->getRealPath());
+
+        $file_contents = str_replace("Copyright (c) $prev_year", "Copyright (c) $current_year", $file_contents);
+
+        file_put_contents($file->getRealPath(), $file_contents);
     }
-
-    $file_contents = file_get_contents($file->getRealPath());
-
-    $file_contents = str_replace("Copyright (c) $prev_year", "Copyright (c) $current_year", $file_contents);
-
-    file_put_contents($file->getRealPath(), $file_contents);
 }
+
+main();
