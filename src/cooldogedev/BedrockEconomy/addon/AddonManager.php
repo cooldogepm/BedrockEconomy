85806da20b7f33b29b837fbddb74cfaa453ae514
Fadhel D <77399231+fadheldev@users.noreply.github.com>
2022-02-23 02:18:41 +0300

    change the level of error reporting and add license headers

--- src/cooldogedev/BedrockEconomy/addon/AddonManager.php
+++ src/cooldogedev/BedrockEconomy/addon/AddonManager.php
@@ -1,5 +1,27 @@
 <?php
 
+/**
+ *  Copyright (c) 2022 cooldogedev
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
 declare(strict_types=1);
 
 namespace cooldogedev\BedrockEconomy\addon;
@@ -10,7 +32,7 @@ use cooldogedev\BedrockEconomy\BedrockEconomy;
 final class AddonManager extends BedrockEconomyOwned
 {
     public const ADDONS_MAP = [
-        "scorehud" => "cooldogedev\\BedrockEconomy\\addon\\scorehud\\ScoreHudAddon",
+        "ScoreHud" => "cooldogedev\\BedrockEconomy\\addon\\scorehud\\ScoreHudAddon",
     ];
 
     /**
@@ -39,7 +61,7 @@ final class AddonManager extends BedrockEconomyOwned
         $addon = new $addonClass($this->plugin);
 
         if (!$addon instanceof Addon) {
-            $this->getPlugin()->getLogger()->critical("Addon " . $addonName . " is not an instance of BedrockEconomy\addon\Addon");
+            $this->getPlugin()->getLogger()->debug("Addon " . $addonName . " is not an instance of BedrockEconomy\addon\Addon");
             return false;
         }
 
@@ -47,17 +69,17 @@ final class AddonManager extends BedrockEconomyOwned
             $addon->getMinimumSupportedBedrockEconomyVersion() !== Addon::SUPPORTED_BEDROCK_VERSION_ALL &&
             version_compare($this->getPlugin()->getDescription()->getVersion(), $addon->getVersion()) < 0
         ) {
-            $this->getPlugin()->getLogger()->critical("Addon " . $addonName . " is not compatible with this version of BedrockEconomy, supported version: " . $addon->getMinimumSupportedBedrockEconomyVersion() . " current version: " . $this->getPlugin()->getDescription()->getVersion());
+            $this->getPlugin()->getLogger()->debug("Addon " . $addonName . " is not compatible with this version of BedrockEconomy, supported version: " . $addon->getMinimumSupportedBedrockEconomyVersion() . " current version: " . $this->getPlugin()->getDescription()->getVersion());
             return false;
         }
 
         if ($this->isAddonLoaded($addonName)) {
-            $this->getPlugin()->getLogger()->critical("Addon " . $addonName . " is already loaded");
+            $this->getPlugin()->getLogger()->debug("Addon " . $addonName . " is already loaded");
             return false;
         }
 
         if (!$addon->isLoadable()) {
-            $this->getPlugin()->getLogger()->critical("Addon " . $addonName . " failed to load");
+            $this->getPlugin()->getLogger()->debug("Addon " . $addonName . " failed to load");
             return false;
         }
 
