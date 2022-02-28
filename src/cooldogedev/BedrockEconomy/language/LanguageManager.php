b40230562905289bb2658ca44419282768e0bc74
LBWBDev <78469040+LBWBDeveloper@users.noreply.github.com>
2022-02-28 14:33:11 +0100

    Added Swissgerman Language (#47)
    ATS (As title says)

--- src/cooldogedev/BedrockEconomy/language/LanguageManager.php
+++ src/cooldogedev/BedrockEconomy/language/LanguageManager.php
@@ -38,7 +38,8 @@ final class LanguageManager
         "vi-VN",
         "zh-TW",
         "zh-CN",
-        "id-ID"
+        "id-ID",
+        "de-CH"
     ];
 
     protected static string $language;
