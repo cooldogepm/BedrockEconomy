a89f3fce85feb1cd4b0c9aa2551b91e3c0374916
Doge <cooldogedev@gmail.com>
2023-12-23 15:16:05 +0300

    fix(LegacyAPI): avoid calling null handlers
    Closes #117

--- src/cooldogedev/BedrockEconomy/api/type/LegacyAPI.php
+++ src/cooldogedev/BedrockEconomy/api/type/LegacyAPI.php
@@ -52,26 +52,31 @@ final class LegacyAPI extends BaseAPI
 
     public function setPlayerBalance(string $username, int $amount, ?Closure $handler = null): void
     {
+        $handler ??= fn () => null;
         BedrockEconomyAPI::CLOSURE()->set(Search::EMPTY, $username, $amount, 0, fn () => $handler(true), fn () => $handler(false));
     }
 
     public function getPlayerBalance(string $username, ?Closure $handler = null): void
     {
+        $handler ??= fn () => null;
         BedrockEconomyAPI::CLOSURE()->get(Search::EMPTY, $username, fn (int $amount) => $handler($amount), fn () => $handler(null));
     }
 
     public function addToPlayerBalance(string $username, int $amount, ?Closure $handler = null): void
     {
+        $handler ??= fn () => null;
         BedrockEconomyAPI::CLOSURE()->add(Search::EMPTY, $username, $amount, 0, fn () => $handler(true), fn () => $handler(false));
     }
 
     public function subtractFromPlayerBalance(string $username, int $amount, ?Closure $handler = null): void
     {
+        $handler ??= fn () => null;
         BedrockEconomyAPI::CLOSURE()->subtract(Search::EMPTY, $username, $amount, 0, fn () => $handler(true), fn () => $handler(false));
     }
 
     public function transferFromPlayerBalance(string $source, string $target, int $amount, ?Closure $handler = null): void
     {
+        $handler ??= fn () => null;
         BedrockEconomyAPI::CLOSURE()->transfer(
             source: [
                 "xuid" => Search::EMPTY,
